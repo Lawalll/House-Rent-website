e95d42a441ef9540d89e47ed044626e3380c2025
Lawalll <kabirasaheed523@gmail.com>
2023-11-09 00:37:22 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const menu =document.getElementById("menu-icon");
+const navbar =document.getElementById('navbar');
+
+document.getElementById('menu').onclick=function(){
+    navbar.classList.toggle('active')
+}
